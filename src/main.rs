cd1aad6de4ecff8a258e354d59468725cd0581a2
thialfi17 <thialfi17@gmail.com>
2024-02-25 18:13:56 +0000

    Converted main.rs to use anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::env::VarError;
-
-use ::config::ConfigError;
+use anyhow::Result;
 use clap::Parser;
 use log::debug;
-use shellexpand::LookupError;
 use simplelog::{ColorChoice, LevelFilter, TermLogger, TerminalMode};
 
 use crate::cli::Cli;
@@ -16,31 +13,7 @@ mod options;
 mod command;
 mod link;
 
-enum Error {
-    Config(ConfigError),
-    Target(LookupError<VarError>),
-}
-
-impl From<ConfigError> for Error {
-    fn from(value: ConfigError) -> Self {
-        Self::Config(value)
-    }
-}
-impl From<LookupError<VarError>> for Error {
-    fn from(value: LookupError<VarError>) -> Self {
-        Self::Target(value)
-    }
-}
-impl core::fmt::Debug for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Config(arg0) => f.debug_tuple("Config").field(arg0).finish(),
-            Self::Target(arg0) => f.debug_tuple("Target").field(arg0).finish(),
-        }
-    }
-}
-
-fn main() -> Result<(), Error> {
+fn main() -> Result<()> {
     let cli = Cli::parse();
     let config = crate::config::Config::new()?;
     let options = Options::new(&cli, &config)?;
@@ -58,7 +31,7 @@ fn main() -> Result<(), Error> {
     );
 
     if options.adopt {
-        unimplemented!();
+        anyhow::bail!("--adopt hasn't been implemented yet!");
     }
 
     debug!("{:?}", options);
